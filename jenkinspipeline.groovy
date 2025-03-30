c5865684b159038b6df5c9f87faed85cc970899d
Anil Pawar <anilpawar8111997@gmail.com>
2025-03-30 16:22:33 +0530

    writting configuration file or pipeline

---
+++ jenkinspipeline.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent  {
+        label 'node1'
+    }
+    stages {
+        stage('pull') { 
+            steps {
+                git 'https://github.com/SurajBele/studentapp.ui.git'
+                echo "pull is successful"
+            }
+        }
+        stage('building') { 
+            steps {
+                sh 'mvn clean package'
+                echo "building is successful"
+            }
+        }
+        stage('testing') { 
+            steps {
+                echo "testing is successful "
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo "Deployment is successful "
+            }
+        }
+    }
+}
